9c34da32d05c8c83139295c0e5155cc5efe0cf65
Ottaliam <ottaliam@gmail.com>
2024-11-10 14:33:37 -0500

    Refactor so that footer is added by js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-document.querySelector('#year').textContent = new Date().getFullYear().toString();
+const footer = `
+  <img src="images/UMSI_signature_vertical_informal_solidblue.jpg" alt="UMSI Logo">
+  <p id="copyright">Copyright &copy; ${new Date().getFullYear().toString()} University of Michigan</p>
+  <p><a href="https://sites.google.com/umich.edu/msiacademicadvising/student-support-resources">UMSI Academic Advising</a></p>
+  <p><a href="https://sites.google.com/umich.edu/msiacademicadvising/academic-advising/connecting-with-an-advisor?authuser=0">Contact Us</a></p>
+`;
+
+document.querySelector('footer').innerHTML = footer;
